fa4f3879d12b5097652b504d496081710a219ecb
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-05-17 17:30:05 -0500

    validation and error composure

--- ground/callFlow.ts
+++ ground/callFlow.ts
@@ -28,28 +28,96 @@ function _getObjValueFromName(obj: object, name: string): CallFlowFn {
 	return val
 }
 
+function _moduleHewSyntaxErrMsg(callFlowStr: string, cfArray: string[], target: string): string {
+	const error = { msg: '' }
+	error.msg += 'invalid call flow: '
+	error.msg += `"${callFlowStr}"`
+	error.msg += ' '
+	error.msg += `<${cfArray.join('.')}>`
+	error.msg += ' '
+	error.msg += `->${target}<-`
+	return error.msg
+}
+
+function _hewModule(callFlowStr: string, cfArray: string[], module: object): object {
+	// init return
+	const callFlow = { module: { ...module } }
+	// rebuild module object
+	for (let i = 0; i < cfArray.length; i++) {
+		// get target name from call chain array
+		const target = cfArray[i]
+		// validate target
+		if (target) {
+			// get index from source object
+			callFlow.module = _getObjValueFromName(callFlow.module, target)
+			// validate
+			if (!callFlow.module) {
+				const msg = _moduleHewSyntaxErrMsg(callFlowStr, cfArray, target)
+				throw new SyntaxError(msg)
+			}
+		}
+	}
+	// return
+	return callFlow.module
+}
+
+function _isValidCallFlowArray(cfArray: string[]): boolean {
+	// cf array must have at least 2 elements
+	if (cfArray.length >= 3) {
+		// validate call flow array strings
+		for (let i = 0; i < cfArray.length; i++) {
+			// get string
+			const str = cfArray[i]
+			// cover a few basics
+			if (!str || str == '.' || str == ' ') {
+				// bad string
+				return false
+			}
+		}
+	} else {
+		return false
+	}
+	// return valid
+	return true
+}
+
+function _hewCallFlowArray(callFlowStr: string): string[] {
+	const cfArray = callFlowStr.split('.')
+	if (!_isValidCallFlowArray(cfArray)) {
+		const error = { msg: '' }
+		error.msg += 'invalid call flow string from: '
+		error.msg += `"${callFlowStr}"`
+		throw new SyntaxError(error.msg)
+	}
+	return cfArray
+}
+
 function _linkCallFlow(callFlowStr: string, callFlow: CallFlow) {
 	// build call chain array
-	const cfArray = callFlowStr.split('.')
+	const cfArray = _hewCallFlowArray(callFlowStr)
 	if (cfArray) {
 		// get function name from end of array
 		const fnName = cfArray.pop()
-		// rebuild module object
-		for (let i = 0; i < cfArray.length; i++) {
-			// get target name from call chain array
-			const target = cfArray[i]
-			// validate target
-			if (target) {
-				// get index from source object
-				callFlow.module = _getObjValueFromName(callFlow.module, target)
-				//source = source[cfArray[i]]
-			}
-		}
+		// hew module
+		callFlow.module = _hewModule(callFlowStr, cfArray, callFlow.module)
 		// call function
 		if (fnName) {
 			const fn = _getObjValueFromName(callFlow.module, fnName)
-			callFlow.fn = fn
+			if (fn) {
+				callFlow.fn = fn
+			} else {
+				const error = { msg: '' }
+				error.msg += 'invalid function name'
+				error.msg += ' '
+				error.msg += `"${fnName}"`
+				error.msg += ' '
+				error.msg += 'from: '
+				error.msg += callFlowStr
+				throw new Error(error.msg)
+			}
 		}
+	} else {
+		throw new SyntaxError('invalid call flow string: ' + callFlowStr)
 	}
 }
 
@@ -57,7 +125,7 @@ function _hewCallFlowContext(): CallFlow {
 	const callFlow: CallFlow = {
 		base: _getModuleBase(),
 		module: _getModuleBase(),
-		fn: () => {}
+		fn: () => { }
 	}
 	return callFlow
 }
