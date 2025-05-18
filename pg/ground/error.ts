2985aecbd508a9d554f3416d666b6798369f302b
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-05-17 21:09:24 -0500

    error...from...

--- pg/ground/error.ts
+++ pg/ground/error.ts
@@ -31,13 +31,15 @@ const msg = {
 		//_hewModule
 		hewModule: (callFlowStr: string, cfArray: string[], target: string): string => {
 			const error = { msg: '' }
-			error.msg += 'invalid call flow:'
-			error.msg += ' '
-			error.msg += `"${callFlowStr}"`
-			error.msg += ' '
-			error.msg += `<${cfArray.join('.')}>`
+			error.msg += 'invalid call flow'
 			error.msg += ' '
 			error.msg += `->${target}<-`
+			error.msg += ' '
+			error.msg += `"${cfArray.join('.')}"`
+			error.msg += ' '
+			error.msg += 'from:'
+			error.msg += ' '
+			error.msg += `"${callFlowStr}"`
 			return error.msg
 		}
 	}
