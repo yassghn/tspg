ee16ba1a9439d59bf4796eb5c988a6281b844b3c
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-05-20 16:27:56 -0500

    testing shallow copy

--- pg/play/experimenotation/api/objects.ts
+++ pg/play/experimenotation/api/objects.ts
@@ -4,6 +4,39 @@
 
 function shallowCopy() {
 	console.log('shallow copy test')
+
+	const cmp = {
+		x: 1,
+		y: 1,
+		z: 1
+	}
+
+	const cmpx = {
+		x: 1,
+		y: 2,
+		z: 3,
+		cmpZ: cmp
+	}
+
+	// copy reference
+	const cmpxEqual = cmpx
+	// shallow copy object (keep nested references)
+	const cmpxCopy = { ...cmpx }
+
+	// equality tests
+	console.log(cmpx == cmpxEqual)
+	console.log(cmpx === cmpxEqual)
+
+	console.log(cmpx == cmpxCopy)
+	console.log(cmpx === cmpxCopy)
+
+	// alter values
+	cmpx.cmpZ.x = 5
+	cmpx.y = 10
+	// cmpZ, i.e. const cmp, refence stays the same
+	console.log(cmpx)
+	console.log(cmpxEqual)
+	console.log(cmpxCopy)
 }
 
 export { shallowCopy }
