017a7ce3c07d5bf0336d1d0e75e62a2fd6437d4e
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-05-23 17:19:58 -0500

    where the fuck is ForegroundColors?! you fucking morons.

--- pg/mutual/io.ts
+++ pg/mutual/io.ts
@@ -2,59 +2,53 @@
  * io.ts
  */
 
+import { WriteStream } from 'fs'
+import { styleText } from 'util'
+
 enum STDLVL {
 	IN,
 	OUT,
 	ERR
 }
 
-interface Emitter {
-	set: string
-	reset: string
+const colors = {
+	red: 'red',
+	default: 'default'
 }
 
-async function _hewEmitter():Promise<Emitter> {
+function _stdTypeColorInfer(stdLvl: STDLVL): ForegroundColors {
 	// init return
-	const emitter: Emitter = { set: '', reset: ''}
-	// create proc to set terminal foreground
-	const set = Bun.spawn(['tput', 'setaf', '1'])
-	// create proc to reset terminal properties
-	const reset = Bun.spawn(['tput', 'sgr0'])
-	// get emitter text values
-	const setColor = await new Response(set.stdout).text()
-	const resetColor = await new Response(reset.stdout).text()
-	// set return vals
-	emitter.set = setColor
-	emitter.reset = resetColor
-	return emitter
-}
-
-async function _applyEmitter(lambda: (...params: any) => {}, ...params: any) {
-	// get emitter
-	const emitter = await _hewEmitter()
-	// update msg string with emitter values
-	const lastIndex = params.length - 1
-	const error = { msg: params[lastIndex] }
-	error.msg = `${emitter.set}${error.msg}${emitter.reset}`
-	params[lastIndex] = error.msg
-	// run lambda
-	lambda(...params)
-}
-
-function _writeStd(msg: string, stdLvl: number) {
+	const infer = { color: '' }
+	// infer color based on STDLVL
 	switch (stdLvl) {
 		case STDLVL.IN:
+			infer.color += ''
 			break;
 		case STDLVL.OUT:
+			infer.color += ''
 			break;
 		case STDLVL.ERR:
-			_applyEmitter(Bun.write, Bun.stderr, msg)
+			infer.color += colors.red
 			break;
 	}
+	// return
+	return infer.color
+}
+
+function _applyEmitter(std: WriteStream & any, stdLvl: STDLVL, str: string): string {
+	const ret = { str: '' }
+	const color = _stdTypeColorInfer(stdLvl)
+	ret.str += styleText(color, str, { stream: std, validateStream: true})
+	return ret.str
+}
+
+function echo(std: WriteStream & any, str: string) {
+	Bun.write(std, str + '\n')
 }
 
 async function _writeError(msg: string) {
-	_writeStd(msg, STDLVL.ERR)
+	const emittedStr = _applyEmitter(Bun.stderr, STDLVL.ERR, msg)
+	echo(Bun.stderr, emittedStr)
 }
 
 const io = {
