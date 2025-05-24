ac8988e47e4d4ca975edf161b7e1ef91ed0de820
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-05-23 19:57:18 -0500

    borked formatting issues from borked devs. goddamned country can barely put together a statically typed overlay that is 100% completely unnecessary and literally makes everything unecessarily difficult. morons.

--- pg/mutual/io.ts
+++ pg/mutual/io.ts
@@ -6,57 +6,57 @@ import { WriteStream } from 'fs'
 import { styleText } from 'util'
 
 enum STDLVL {
-	IN,
-	OUT,
-	ERR
+    IN,
+    OUT,
+    ERR
 }
 
 const colors = {
-	red: 'red',
-	default: 'default'
+    red: 'red',
+    default: 'default'
 }
 
 function _stdTypeColorInfer(stdLvl: STDLVL): ForegroundColors {
-	// init return
-	const infer = { color: '' }
-	// infer color based on STDLVL
-	switch (stdLvl) {
-		case STDLVL.IN:
-			infer.color += ''
-			break;
-		case STDLVL.OUT:
-			infer.color += ''
-			break;
-		case STDLVL.ERR:
-			infer.color += colors.red
-			break;
-	}
-	// return
-	return infer.color
+    // init return
+    const infer = { color: '' }
+    // infer color based on STDLVL
+    switch (stdLvl) {
+        case STDLVL.IN:
+            infer.color += ''
+            break
+        case STDLVL.OUT:
+            infer.color += ''
+            break
+        case STDLVL.ERR:
+            infer.color += colors.red
+            break
+    }
+    // return
+    return infer.color
 }
 
 function _applyEmitter(std: WriteStream & any, stdLvl: STDLVL, str: string): string {
-	const ret = { str: '' }
-	const color = _stdTypeColorInfer(stdLvl)
-	ret.str += styleText(color, str, { stream: std, validateStream: true})
-	return ret.str
+    const ret = { str: '' }
+    const color = _stdTypeColorInfer(stdLvl)
+    ret.str += styleText(color, str, { stream: std, validateStream: true })
+    return ret.str
 }
 
 function echo(std: WriteStream & any, str: string) {
-	Bun.write(std, str + '\n')
+    Bun.write(std, str + '\n')
 }
 
 async function _writeError(msg: string) {
-	const emittedStr = _applyEmitter(Bun.stderr, STDLVL.ERR, msg)
-	echo(Bun.stderr, emittedStr)
+    const emittedStr = _applyEmitter(Bun.stderr, STDLVL.ERR, msg)
+    echo(Bun.stderr, emittedStr)
 }
 
 const io = {
-	std: {
-		err: (msg: string) => {
-			_writeError(msg)
-		}
-	}
+    std: {
+        err: (msg: string) => {
+            _writeError(msg)
+        }
+    }
 }
 
 export default io
