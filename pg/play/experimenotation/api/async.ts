12d87e3e5a20473718b26c874bdb8918505360bb
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-05-25 22:31:15 -0500

    its probably gonna happen

---
+++ pg/play/experimenotation/api/async.ts
@@ -0,0 +1,14 @@
+/**
+ * async.ts
+ */
+
+function awaitIsBrokenAndThesePeopleFuckingSuck() {
+
+    /**
+     * stop breaking shit and doing retardation
+     * or i am going to fucking slaughter all of you.
+     */
+
+}
+
+export { awaitIsBrokenAndThesePeopleFuckingSuck }
