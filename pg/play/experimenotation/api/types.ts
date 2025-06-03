fe76d7fdc9b43fa3948475f76b589d51a855ec22
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-06-02 19:11:17 -0500

    interface extensions

--- pg/play/experimenotation/api/types.ts
+++ pg/play/experimenotation/api/types.ts
@@ -6,4 +6,37 @@ function theBasics() {
 	Bun.write(Bun.stdout, 'the basics\n')
 }
 
-export { theBasics }
+function extendInterface() {
+	interface BASE {
+		prop1: string
+		prop2: string
+	}
+
+	interface EXTENSION extends BASE {
+		prop3: number
+	}
+
+	interface USEEXTENSION {
+		prop4: EXTENSION
+	}
+
+	const obj: EXTENSION = {
+		prop1: '1',
+		prop2: '2',
+		prop3: 3
+	}
+
+	const obj2: USEEXTENSION = {
+		prop4: {
+			prop1: '1',
+			prop2: '2',
+			prop3: 3
+		}
+	}
+
+	console.dir(obj)
+	console.log(typeof obj)
+	console.dir(obj2)
+}
+
+export { theBasics, extendInterface }
