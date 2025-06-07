0e3fedaa61322defeb4b7f1413396c368a5826ba
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-06-07 17:55:27 -0500

    imports regex testing

--- pg/play/experimenotation/api/regex.ts
+++ pg/play/experimenotation/api/regex.ts
@@ -3,7 +3,22 @@
  */
 
 function imports() {
-    console.log('imports')
+    console.log('imports\n')
+    const importsString =
+    `
+    import config from '@test/config'
+    import { test, test1, test2 } from '@test/test'
+    import jsonData from '.../dir/dir/test.json' assert { type: 'json' }
+    import apiObj from 'node:apiModule'
+    `
+    console.log('original imports string:')
+    console.log(importsString)
+
+    const regex = /\'\@/mgi
+    const newString = importsString.replaceAll(regex, '\'./')
+
+    console.log('imports string after applying regex:')
+    console.log(newString)
 }
 
 export { imports }
